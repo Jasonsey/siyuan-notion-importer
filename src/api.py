868321de977ebdefbeae43e04efcd303ba8cdbbf
Max Lin <jason.max.lin@outlook.com>
2025-02-11 15:45:54 +0800

    feat: optimize the speed of preprocessing

--- src/api.py
+++ src/api.py
@@ -8,25 +8,27 @@
 #
 # =============================================================================
 """api.py"""
+from asyncio import Semaphore
 from typing import *
 from dataclasses import dataclass, field
 import json
-import requests
 from pathlib import Path
+
+import aiohttp
 
 
 @dataclass
 class Api:
-    notebook_name: str = 'test-notion'
+    notebook_name: str = 'notion'
     data_home: str = "/Users/max/SiYuan/data"
     base_url: str = "http://127.0.0.1:6806"
     headers: Dict[str, str] = field(default_factory=lambda: {"Content-Type": "application/json"})
     _notebook_home: Optional[str] = None
+    _sem: Semaphore = Semaphore(1)
 
-    @property
-    def notebook_home(self):
+    async def notebook_home(self):
         if self._notebook_home is None:
-            notebooks = self.list_notebooks()
+            notebooks = await self.list_notebooks()
             for notebook in notebooks:
                 if notebook['name'] == self.notebook_name:
                     self._notebook_home = str(Path(self.data_home) / notebook['id'])
@@ -34,68 +36,89 @@ class Api:
                 raise RuntimeError(f"No notebook named {self.notebook_name}")
         return self._notebook_home
 
-    def get_all_sy_files(self):
-        path = Path(self.notebook_home)
+    async def get_all_sy_files(self):
+        path = Path(await self.notebook_home())
         names = list(path.glob("**/*.sy"))
         return names
 
-    def list_notebooks(self):
-        url = f"{self.base_url}/api/notebook/lsNotebooks"
-        res = requests.post(url, headers=self.headers).json()
+    async def list_notebooks(self):
+        async with self._sem:
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                url = f"{self.base_url}/api/notebook/lsNotebooks"
+                async with session.post(url) as response:
+                    res = await response.json()
         if res['code'] != 0:
             raise RuntimeError(f"Error listing notebooks: {res['msg']}")
         else:
             return res['data']['notebooks']
 
-    def get_filepath_by_id(self, idx: str):
-        url = f"{self.base_url}/api/filetree/getPathByID"
-        payload = json.dumps({"id": idx})
-        res = requests.post(url, data=payload, headers=self.headers).json()
+    async def get_filepath_by_id(self, idx: str):
+        async with self._sem:
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                url = f"{self.base_url}/api/filetree/getPathByID"
+                payload = json.dumps({"id": idx})
+                async with session.post(url, data=payload) as response:
+                    res = await response.json()
         if res['code'] == 0:
             # success
-            path = Path(self.notebook_home) / ('.' + res['data'])
+            path = Path(await self.notebook_home()) / ('.' + res['data'])
             return path
         else:
             raise ValueError(res['msg'])
 
-    def update_block(self, data: str, idx: str):
+    async def update_block(self, data: str, idx: str):
         """使用markdown更新块内容"""
-        url = f"{self.base_url}/api/block/updateBlock"
-        payload = json.dumps({"data": data, "dataType": "markdown", "id": idx})
-        res = requests.post(url, data=payload, headers=self.headers).json()
+        async with self._sem:
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                url = f"{self.base_url}/api/block/updateBlock"
+                payload = json.dumps({"data": data, "dataType": "markdown", "id": idx})
+                async with session.post(url, data=payload) as response:
+                    res = await response.json()
         if res['code'] != 0:
             raise RuntimeError(f"Error updating block: {res['msg']}")
 
-    def get_block_kramdown(self, idx: str):
-        url = f"{self.base_url}/api/block/getBlockKramdown"
-        payload = json.dumps({"id": idx})
-        res = requests.post(url, data=payload, headers=self.headers).json()
+    async def get_block_kramdown(self, idx: str):
+        async with self._sem:
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                url = f"{self.base_url}/api/block/getBlockKramdown"
+                payload = json.dumps({"id": idx})
+                async with session.post(url, data=payload) as response:
+                    res = await response.json()
         if res['code'] != 0:
             raise RuntimeError(f"Error getting block kramdown: {res['msg']}")
         else:
             return res['data']["kramdown"]
 
-    def insert_block(self, data: str, next_id="", previous_id="", parent_id=""):
+    async def insert_block(self, data: str, next_id="", previous_id="", parent_id=""):
         """插入markdown格式的块"""
-        url = f"{self.base_url}/api/block/insertBlock"
-        payload = json.dumps({"data": data, "nextID": next_id, "previousID": previous_id, "parentID": parent_id, "dataType": "markdown"})
-        res = requests.post(url, data=payload, headers=self.headers).json()
+        async with self._sem:
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                url = f"{self.base_url}/api/block/insertBlock"
+                payload = json.dumps({"data": data, "nextID": next_id, "previousID": previous_id, "parentID": parent_id, "dataType": "markdown"})
+                async with session.post(url, data=payload) as response:
+                    res = await response.json()
         if res['code'] != 0:
             raise RuntimeError(f"Error inserting block: {res['msg']}")
         else:
             return res['data']['doOperations']['id']
 
-    def delete_block(self, idx: str):
-        url = f"{self.base_url}/api/block/deleteBlock"
-        payload = json.dumps({"id": idx})
-        res = requests.post(url, data=payload, headers=self.headers).json()
+    async def delete_block(self, idx: str):
+        async with self._sem:
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                url = f"{self.base_url}/api/block/deleteBlock"
+                payload = json.dumps({"id": idx})
+                async with session.post(url, data=payload) as response:
+                    res = await response.json()
         if res['code'] != 0:
             raise RuntimeError(f"Error deleting block: {res['msg']}")
 
-    def get_child_blocks(self, idx: str):
-        url = f"{self.base_url}/api/block/getChildBlocks"
-        payload = json.dumps({"id": idx})
-        res = requests.post(url, data=payload, headers=self.headers).json()
+    async def get_child_blocks(self, idx: str):
+        async with self._sem:
+            async with aiohttp.ClientSession(headers=self.headers) as session:
+                url = f"{self.base_url}/api/block/getChildBlocks"
+                payload = json.dumps({"id": idx})
+                async with session.post(url, data=payload) as response:
+                    res = await response.json()
         if res['code'] != 0:
             raise RuntimeError(f"Error getting child blocks: {res['msg']}")
         else:
--- src/main.py
+++ src/main.py
@@ -10,9 +10,14 @@
 """main"""
 import re
 import json
+from typing import Optional
+
+import asyncio
 from tqdm import tqdm
 
 from api import Api
+
+api = Api()
 
 
 def update_node_paragraph(data: str):
@@ -21,7 +26,9 @@ def update_node_paragraph(data: str):
     if '\\$' in data:
         data = re.sub(r"\\(\\*)", r"\1", data)
     # img optimize show
-    data = re.sub(r"!?\[(.*?)]\((.*?)\.(bmp|jpg|png|tif|gif|pcx|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|raw|WMF|webp|jpeg)\)", r"![\1](\2.\3)", data)
+    data = re.sub(
+        r"!?\[(.*?)]\((.*?)\.(bmp|jpg|png|tif|gif|pcx|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|raw|WMF|webp|jpeg)\)",
+        r"![\1](\2.\3)", data)
     return data
 
 
@@ -36,11 +43,11 @@ def update_node_blockquote(data: str):
     if "[!important]" in data or "[!info]" in data:
         res = []
 
-        important_data = re.findall(r"> \[!important](.*?)(?:\[!.*?]|{: id=.*? updated=.*?})", data, flags=re.DOTALL)
-        important_data = [re.sub(r"\n?(.*?)(?:\n> )?$", r"\1", item) for item in important_data]
+        important_data = re.findall(r"> \[!important](.*?)(?:\[!.*?]|{: .*?=.*? .*?=.*?})", data, flags=re.DOTALL)
+        important_data = [item.strip() for item in important_data]
         res.extend(important_data)
 
-        info_data = re.findall(r"> \[!info](.*?)(?:\[!.*?]|{: id=.*? updated=.*?})", data, flags=re.DOTALL)
+        info_data = re.findall(r"> \[!info](.*?)(?:\[!.*?]|{: .*?=.*? .*?=.*?})", data, flags=re.DOTALL)
         info_data = [re.sub(r"\n?(.*?)(?:\n> )?$", r"\1", item) for item in info_data]
         for idx, item in enumerate(info_data):
             # url
@@ -57,53 +64,58 @@ def update_node_blockquote(data: str):
     return data
 
 
-def update_data(data: dict):
+async def update_data(data: dict):
     """更新NodeParagraph"""
-    api = Api()
-
     if data['Type'] == "NodeParagraph":
         # update
         idx = data['ID']
-        markdown_data = api.get_block_kramdown(idx)
+        markdown_data = await api.get_block_kramdown(idx)
         markdown_data = update_node_paragraph(markdown_data)
-        api.update_block(markdown_data, idx=idx)
+        await api.update_block(markdown_data, idx=idx)
     elif data['Type'] == "NodeMathBlock":
         # update math block
         idx = data['ID']
-        markdown_data = api.get_block_kramdown(idx)
+        markdown_data = await api.get_block_kramdown(idx)
         markdown_data = update_node_math_block(markdown_data)
-        api.update_block(markdown_data, idx=idx)
+        await api.update_block(markdown_data, idx=idx)
     elif data['Type'] == "NodeBlockquote":
         # update quote
         idx = data['ID']
-        markdown_data = api.get_block_kramdown(idx)
+        markdown_data = await api.get_block_kramdown(idx)
         markdown_data = update_node_blockquote(markdown_data)
-        api.update_block(markdown_data, idx=idx)
+        await api.update_block(markdown_data, idx=idx)
     elif "Children" not in data:
         pass
     else:
         for child in data['Children']:
-            update_data(child)
+            await update_data(child)
 
 
-def main():
-    api = Api()
+async def update_file_with_bar(data: dict, bar: Optional[tqdm] = None):
+    await update_data(data)
+    if bar is not None:
+        bar.update()
 
-    path = api.get_filepath_by_id("20250210140717-bh2fko2")
+
+async def main():
+    path = await api.get_filepath_by_id("20250211145209-ncpw7tz")
     data = json.load(open(path))
-    update_data(data)
+    await update_data(data)
 
     data = json.load(open(path))
     print(json.dumps(data, indent=2, ensure_ascii=False))
 
 
-def main2():
-    api = Api()
-    files = api.get_all_sy_files()
-    for file in tqdm(files):
+async def main2():
+    files = await api.get_all_sy_files()
+    bar = tqdm(total=len(files), desc="updating file")
+    tasks = []
+    for file in files:
         data = json.load(open(file))
-        update_data(data)
+        task = asyncio.create_task(update_file_with_bar(data, bar))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    main2()
+    asyncio.run(main2())
